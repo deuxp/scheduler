da33078af4e983a1e10ee333d5c007a1b2e0ea84
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-22 12:31:43 -0500

    buttons built and tested using storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   let buttonClass = 'button'
-
-   if (props.confirm) {
-      buttonClass += ' button--confirm'
-   }
-   if (props.danger) {
-      buttonClass += ' button--danger'
-   }
-   
+   const buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
    
    return <button onClick={props.onClick} 
                   className={buttonClass}
