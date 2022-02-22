76a5dce0be90eda41165e93a2e39a53d15756175
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-02-21 23:47:17 -0500

    conditional classes for the button in storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,18 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = 'button'
+
+   if (props.confirm) {
+      buttonClass += ' button--confirm'
+   }
+   if (props.danger) {
+      buttonClass += ' button--danger'
+   }
+   
+   
+   return <button onClick={props.onClick} 
+                  className={buttonClass}
+                  disabled={props.disabled}
+                  >{props.children}</button>;
 }
