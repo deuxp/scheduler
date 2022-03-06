54efdf3f510130e83fac91c4f626d6285b593bb0
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-03-05 23:44:11 -0500

    implement useVisualMode in <Appointment/> to conditionally render <Form/>

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import Empty from './Empty'
-import './styles.scss'
-import Header from './Header'
-import Show from './Show'
+import React from 'react';
+import './styles.scss';
+import Empty from './Empty';
+import Header from './Header';
+import Show from './Show';
+import Form from './Form';
+import { useVisualMode } from 'hooks/useVisualMode';
 
-// props: (1) time (2) interview:truthy->render<Show/> || <empty/> 
+export default function Appointment({ time, interview, _id }) {
 
-export default function Appointment({ time, interview, student }) {
-  const renderInterview =
-    <Show 
-      interview={interview} 
-      className='appointment__add' 
-    />
-  const renderEmpty = <Empty className='appointment__add' />
+  const EMPTY = 'EMPTY',
+        SHOW = 'SHOW',
+        CREATE = 'CREATE',
+        { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY)
+  
+  const renderShow = <Show 
+    interview={interview} 
+    className='appointment__add' />
+
+  const renderEmpty = <Empty 
+    className='appointment__add' 
+    onAdd={() => transition(CREATE)} />
+
+  const renderForm = <Form 
+    interviewers={[]}
+    onSave={() => console.log('onSave')}
+    onCancel={() => back()} />
   
   return (
     <div className='appointment'>
       <Header time={time} />
-      {interview ? renderInterview : renderEmpty}
+      {mode === EMPTY && renderEmpty}
+      {mode === SHOW && renderShow}
+      {mode === CREATE && renderForm}
     </div>
   )
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react'
+import { useState } from 'react'
 
 export function useVisualMode(initState) {
   const [mode, setMode] = useState(initState)
