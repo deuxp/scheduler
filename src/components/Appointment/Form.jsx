3b69dd2454d64be4cb0e4107a4bbd57a07cdf6d8
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-03-11 10:34:46 -0500

    implement *Required field guard clause for onSave event listener. This was causing an error when the form was saved with no data present

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,6 +5,7 @@ import InterviewerList from 'components/InterviewerList'
 function Form({onSave, onCancel, interviewers, studentName, interviewerID}) {
   const [student, setStudent] = useState(studentName || '')
   const [interviewer, setInterviewer] = useState(interviewerID || null)
+  const [errorMessage, setErrorMessage] = useState('')
 
   const reset = () => {
     setStudent('')
@@ -17,34 +18,39 @@ function Form({onSave, onCancel, interviewers, studentName, interviewerID}) {
   
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off"
-          onSubmit={event => event.preventDefault()}
-    >
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        value={student}
-        onChange={e => setStudent(e.target.value)}
-      />
-    </form>
-    <InterviewerList 
-      interviewers={interviewers}
-      value={interviewer}
-      onChange={setInterviewer}
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button> 
+      <section className="appointment__card-left">
+        <form autoComplete="off"
+              onSubmit={event => event.preventDefault()} 
+        >
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={e => setStudent(e.target.value)}
+          />
+          <p>{errorMessage}</p>
+        </form>
+        <InterviewerList 
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => {
+            if ( student.length === 0 ) setErrorMessage('*Required: Student name cannot be empty')
+            else if ( !interviewer ) setErrorMessage('*Required: Must select a mentor')
+            else onSave(student, interviewer)
+            }}>Save</Button> 
 
-      
-    </section>
-  </section>
-</main>
+          
+        </section>
+      </section>
+  </main>
 
   )
 }
